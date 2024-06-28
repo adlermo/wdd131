248e76f4f5acc9d4f9dbdd6b3c70964057e77f84
Adler Mesquita Orteney <adlerorteney@gmail.com>
2024-06-28 00:29:44 -0300

    Fixed hidden menu on width > 880

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,11 +3,14 @@ const navMenu = document.querySelector('#navMenu');
 
 toggleNavMenu.addEventListener('change', () => {
   // Open menu checked, open menu
-  navMenu.toggleAttribute('hidden');
+  if (window.innerWidth <= 880) navMenu.toggleAttribute('hidden');
 });
 
 navMenu.addEventListener('click', () => {
   // Menu option chosen, checking it
-  navMenu.toggleAttribute('hidden');
-  //   openMenu.firstChild.nextElementSibling.innerText = 'Menu';
+  if (window.innerWidth <= 880) navMenu.toggleAttribute('hidden');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 880) navMenu.removeAttribute('hidden');
 });
