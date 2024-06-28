118dd01abe0698d65eed4847ca6b747d681a926a
Adler Mesquita Orteney <adlerorteney@gmail.com>
2024-06-28 00:07:27 -0300

    trying to remove auto closing tags html

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,17 @@ const today = new Date();
 
 lastModified.innerHTML = `Last Modified in ${document.lastModified}`;
 copyrightYear.innerHTML = `©${today.getFullYear()}`;
+
+const toggleNavMenu = document.querySelector('#toggleNavMenu');
+const navMenu = document.querySelector('#navMenu');
+
+toggleNavMenu.addEventListener('change', () => {
+  // Open menu checked, open menu
+  navMenu.toggleAttribute('hidden');
+});
+
+navMenu.addEventListener('click', () => {
+  // Menu option chosen, checking it
+  navMenu.toggleAttribute('hidden');
+  //   openMenu.firstChild.nextElementSibling.innerText = 'Menu';
+});
